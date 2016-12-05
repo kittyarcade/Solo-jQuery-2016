4139b7baaa4f02b8ee81bb63602e6f2f155cd4a1
Amanda Jopp <aclaire488@gmail.com>
2016-12-05 15:34:03 -0600

    definitely need help.

---
+++ client.js
@@ -0,0 +1,19 @@
+console.log('yup');
+
+
+
+
+
+$(document).ready(function() {
+console.log('yes');
+
+$('.color-button').on('click',function(){
+  $(this).data('red');
+  console.log('red');
+});
+
+
+
+
+
+});//end doc ready
