475dae404d1a3d3deb6a8e353ec330a09dd769e5
Amanda Jopp <aclaire488@gmail.com>
2016-12-05 19:29:15 -0600

    resubmit

--- client.js
+++ client.js
@@ -1,19 +1,44 @@
-console.log('yup');
+console.log('sourced');
 
+var red = 0;
 
+var green = 0;
+
+var yellow = 0;
+
+var blue = 0;
 
 
 
 $(document).ready(function() {
-console.log('yes');
 
+  //get buttons to respond to their individual color
 $('.color-button').on('click',function(){
-  $(this).data('red');
-  console.log('red');
+  var color = $(this).attr('data-color');
+  console.log(color);
+
+  //append the div for the color-cube to the DOM
+  $('.container').append("<div class='color-cube'" + color + "></div>");
+
+
+//when button is pressed, total number increases with corresponding color
+  if(color == 'green'){
+  green++;
+  $('#green').html("Total green:" + green);
+}
+  else if (color == 'red'){
+    red++;
+    $('#red').html('Total red: ' + red);
+}
+  else if (color == 'yellow'){
+    yellow++;
+    $('#yellow').html('Total yellow: ' + yellow);
+}
+  else if (color == 'blue'){
+    blue++;
+    $('#blue').html('Total blue: ' + blue);
+  }
+
 });
-
-
-
-
 
 });//end doc ready
